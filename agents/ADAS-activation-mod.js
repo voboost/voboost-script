231693943c887861669bd81688edaf72f657055f
aliaksandr <kovalev.sasha82@gmail>
2025-11-26 21:54:42 +0300

    add adas activate script

---
+++ agents/ADAS-activation-mod.js
@@ -0,0 +1,37 @@
+let BaiduProviderUtil = null;
+
+function doQuerySubscribeInfoHook() {
+  try {
+
+    BaiduProviderUtil.doQuerySubscribeInfo.implementation = function (/* unused */) {
+      return "{\"expireStatus\":\"0\",\"isMqtt\":false,\"remainDays\":\"30\",\"subscriptionStatus\":\"1\"}";
+    };
+  } catch (e) {
+    console.log("[-] Error in doQuerySubscribeInfo: " + e.message);
+    console.log(e.stack);
+  }
+}
+
+function doQueryNOALearnInfoHook() {
+  try {
+    BaiduProviderUtil.doQueryNOALearnInfo.implementation = function (/* unused */) {
+      return ""; //TODO: find out the answer format
+    };
+  } catch (e) {
+    console.log("[-] Error in doQueryNOALearnInfo: " + e.message);
+    console.log(e.stack);
+  }
+}
+
+function init() {
+  BaiduProviderUtil = Java.use("com.qinggan.app.vehiclesetting.fragments.driveassistance.adas.BaiduProviderUtil");
+}
+
+function main() {
+  Init();
+
+  doQuerySubscribeInfoHook();
+  doQueryNOALearnInfoHook()
+}
+
+Java.perform(() => { main(); });
